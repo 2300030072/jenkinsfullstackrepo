a5c722e8dc5c112c4dfc14434d680520f127395d
a2300030072 <2300030072@kluniversity.in>
2025-09-11 07:48:16 +0530

    Changes made

--- Employee/src/components/config.js
+++ Employee/src/components/config.js
@@ -1,5 +1,5 @@
 const config = {
-  url: "http://localhost:2030/springbootemployeeapi/employeeapi"   // adjust if backend runs on a different port
+  url: "http://localhost:2006/springbootemployeeapi/employeeapi"
 };
-
 export default config;
+
