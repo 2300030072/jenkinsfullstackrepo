b331a80e108cb87885a0acee639a8ff500b14be5
a2300030072 <2300030072@kluniversity.in>
2025-09-11 06:38:38 +0530

    Changes made

--- Employee/src/components/config.js
+++ Employee/src/components/config.js
@@ -1,5 +1,5 @@
 const config = {
-  url: "http://localhost:2030/employeeapi"   // adjust if backend runs on a different port
+  url: "http://localhost:2030/EmployeeManagement"   // adjust if backend runs on a different port
 };
 
 export default config;
