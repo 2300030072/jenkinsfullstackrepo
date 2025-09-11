9b19d73939a628a210f18839136642478d252fd4
a2300030072 <2300030072@kluniversity.in>
2025-09-11 07:53:00 +0530

    changes made

--- EmployeeManagement/src/main/java/com/klef/demo/controller/EmployeeController.java
+++ EmployeeManagement/src/main/java/com/klef/demo/controller/EmployeeController.java
@@ -12,7 +12,7 @@ import com.klef.demo.service.EmployeeService;
 
 @RestController
 @RequestMapping("/employeeapi")
-@CrossOrigin(origins = "http://localhost:2030")
+@CrossOrigin(origins = "*")
 public class EmployeeController {
 
     @Autowired
