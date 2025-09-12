40ad46a67f904bb1c0f2276d34a7aa40f2b7a243
a2300030072 <2300030072@kluniversity.in>
2025-09-12 11:03:15 +0530

    SpringBootServletInitializer is added

--- EmployeeManagement/src/main/java/com/klef/demo/EmployeeManagementApplication.java
+++ EmployeeManagement/src/main/java/com/klef/demo/EmployeeManagementApplication.java
@@ -4,10 +4,11 @@ import java.beans.BeanProperty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class EmployeeManagementApplication {
+public class EmployeeManagementApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementApplication.class, args);
